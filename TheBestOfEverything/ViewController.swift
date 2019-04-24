f1a741d9a110ee284ad817486576fff2d82be60a
Maciek Czarnik <czajnikowski@gmail.com>
2019-04-24 16:37:13 +0200

    Add initial files

---
+++ TheBestOfEverything/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheBestOfEverything
+//
+//  Created by Maciek on 24/04/2019.
+//  Copyright © 2019 mczarnik.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
